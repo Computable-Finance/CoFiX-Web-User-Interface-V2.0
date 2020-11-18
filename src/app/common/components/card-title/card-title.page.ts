5f5b17e69d7f4ac811307c3f615503ccc00c0dc9
foxgem <jianhgreat@hotmail.com>
2020-11-18 10:50:09 +0800

    refactor: 💡 remove useless code in card title

--- src/app/common/components/card-title/card-title.page.ts
+++ src/app/common/components/card-title/card-title.page.ts
@@ -1,16 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Utils } from '../../utils';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-title',
   templateUrl: './card-title.page.html',
   styleUrls: ['./card-title.page.scss'],
 })
-export class CardTitlePage implements OnInit {
+export class CardTitlePage {
   @Input() cardTitle: string;
   @Input() cardSubTitle: string;
   @Input() alertTitle: string;
   @Input() alertContent: string;
-  constructor(private utils: Utils) {}
-  ngOnInit() {}
+
+  constructor() {}
 }
