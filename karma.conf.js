76c417e3602f42ea4cb8ab62a9ad5f6fa88608c6
Feng Yu <abcfy2@163.com>
2020-11-20 13:35:35 +0800

    ci: 🎡 Add test report

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const path = require('path');
+const reportsDir = path.join(__dirname, 'reports');
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,22 +11,27 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-mocha-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: path.join(reportsDir, 'coverage'),
+      reports: ['html', 'lcovonly', 'text-summary', 'json-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    junitReporter: {
+      outputDir: path.join(reportsDir, 'junit')
+    }
   });
 };
