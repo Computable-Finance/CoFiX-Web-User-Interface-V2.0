b97b7553f58a4f5955334dfe0f00193af5beb750
胡伟红 <weihong0427@163.com>
2020-12-22 08:18:28 +0800

    fix: 🐛 improve multiple CSS issues on mobile
    improve multiple CSS issues and change 'learn more' btn title of warning
    modal on mobile
    
    ✅ Closes: #94

--- src/app/liquid/warning/warning-detail/warning-detail.page.ts
+++ src/app/liquid/warning/warning-detail/warning-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription, fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-warning-detail',
@@ -9,10 +11,30 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class WarningDetailPage implements OnInit {
   constructor(private modalController: ModalController) {}
+  private resizeSubscription: Subscription;
 
-  ngOnInit() {}
+  moreDetailTitle: string = 'more_details';
+
+  ngOnInit() {
+    console.log('123');
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(100))
+      .subscribe((event) => {
+        this.changeTitle();
+      });
+    this.changeTitle();
+  }
+
+  changeTitle() {
+    if (window.innerWidth < 500) {
+      this.moreDetailTitle = 'more_details_short';
+    } else {
+      this.moreDetailTitle = 'more_details';
+    }
+  }
 
   async close() {
+    this.resizeSubscription.unsubscribe();
     this.modalController.dismiss({ knownRisk: true });
   }
   learnMore() {
