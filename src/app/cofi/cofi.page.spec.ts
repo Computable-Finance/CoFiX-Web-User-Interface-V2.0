81767d5c152d4147a8b5e1a3aea99d97a00b171c
胡伟红 <weihong0427@163.com>
2021-04-30 16:00:05 +0800

    fix: 🐛 unit test error

--- src/app/cofi/cofi.page.spec.ts
+++ src/app/cofi/cofi.page.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModalController } from '@ionic/angular';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MockDirective, MockPipe, MockProvider } from 'ng-mocks';
 import { CofiXService } from 'src/app/service/cofix.service';
@@ -32,6 +33,8 @@ describe('CofiPage', () => {
             return currentAccount;
           },
         }),
+
+        MockProvider(ModalController),
         MockProvider(BalanceTruncatePipe),
         MockProvider(EventBusService),
         MockProvider(Utils),
